a05657be325e79c690251a1dedea48db30bc3247
Lars Verspohl <larsverspohl@gmail.com>
2013-07-07 20:27:00 +0000

    Update 3 web.js

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var fs = require('fs');
 
-var buffer = new Buffer(fs.readFileSync('index.html'));
+var mybuffer = fs.readFileSync('index.html');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString());
+  response.send(mybuffer.toString());
 });
 
 var port = process.env.PORT || 5000;
