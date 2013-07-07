e8b11635fcbca287e4b46e6821edc8a633fb7a3e
Lars Verspohl <larsverspohl@gmail.com>
2013-07-07 21:09:28 +0000

    Update 5 web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var mybuffer = fs.readFileSync('index.html');
+  var mybuffer = fs.readFileSync("index.html");
   response.send(mybuffer.toString());
 });
 
